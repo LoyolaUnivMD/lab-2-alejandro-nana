a129222cb64ed809e8282141125938981bb64302
Nana Osei-Darko <92757332+LGGE00@users.noreply.github.com>
2024-01-31 17:36:35 -0500

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -16,30 +16,31 @@ class Lab2 {
         long prize = 225938745L;
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
-
+// Program states the purpose to the user
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
-        
+        //Program request the user Name
         System.out.print("What's your name? ");
         String customerName = input.nextLine();
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 6; j++) {
-                int num = rand.nextInt(100);
-                if (num < 10) {
+        for (int i = 0; i < 10; i++) { //Ten rows of random numbers are set
+            for (int j = 0; j < 6; j++) { //six number set to be in each row
+                int num = rand.nextInt(100); // A random number out of 100 is pulled 
+                if (num < 10) {  //if the number is less than 10, a zero is placed in front of it
                     System.out.print("0" + num + " ");
-                } else {
+                } else { // if anything else the number runs
                     System.out.print(num + " ");
                 }
             }
             System.out.print("\n");
         }
-
+//program prints out the good luck message and fetches the customers name variable
         System.out.println("\n------------------------------------");
-        System.out.println("Good luck " + customerName + "!");
+        System.out.println("Good luck " + customerName + "!"); 
         System.out.println("Estimated Jackpot:");
-        System.out.println("$" + prize);
+        System.out.println("$" + prize); //the prize variable is pulled
         System.out.println("------------------------------------");
         System.out.println("\n\n\n\nThank you for buying a lottery ticket from Nana's and Ale's program :)");
     }
 }
+//The final message is sent out, with the estimated prize amount, users name, and ends with thanking the user
