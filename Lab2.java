82238943ad7052ae6e0e6a025189052f98d15be4
aescorcia05 <156947111+aescorcia05@users.noreply.github.com>
2024-01-31 17:47:06 -0500

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -1,44 +1,55 @@
-// Programmers:  [your names here]
+// Programmers:  Nana and Alejandro 
 // Course:  CS 212
-// Due Date:
-// Lab Assignment:
-// Problem Statement:
-// Data In:
-// Data Out:
-// Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
+// Due Date: 02/04/24
+// Lab Assignment: 2
+// Problem Statement: Your program needs to prompt the player for their name and generate 10 random lottery tickets
+// Data In: Customer name
+// Data Out: Lottery tickets and prize
 
 import java.util.Scanner;
 import java.util.Random;
 
 class Lab2 {
     public static void main(String[] args) {
+        // Declaring prize
         long prize = 225938745L;
+
+        // Creating tools for input and random numbers
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
-// Program states the purpose to the user
+        
+        // Outputting purpose to the user
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
-        //Program request the user Name
+        
+        // Requesting the customer's name
         System.out.print("What's your name? ");
         String customerName = input.nextLine();
+        customerName = customerName.strip();
+        
+        
+        for (int i = 0; i < 10; i++) { // Ten tickets are going to be outputted
 
-        for (int i = 0; i < 10; i++) { //Ten rows of random numbers are set
-            for (int j = 0; j < 6; j++) { //six number set to be in each row
+            // Generating a single ticket
+            for (int j = 0; j < 6; j++) {
                 int num = rand.nextInt(100); // A random number out of 100 is pulled 
-                if (num < 10) {  //if the number is less than 10, a zero is placed in front of it
+                
+                // If the number is less than 10, a zero is placed in front of it and then added to the ticket
+                if (num < 10) { 
                     System.out.print("0" + num + " ");
-                } else { // if anything else the number runs
+                } else { // If it is anything else the number is simply added to the ticket
                     System.out.print(num + " ");
                 }
+                
             }
             System.out.print("\n");
         }
-//program prints out the good luck message and fetches the customers name variable
+        
+        // Program prints out a good luck message and outputs the estimated jackpot.
         System.out.println("\n------------------------------------");
         System.out.println("Good luck " + customerName + "!"); 
         System.out.println("Estimated Jackpot:");
-        System.out.println("$" + prize); //the prize variable is pulled
+        System.out.println("$" + prize);
         System.out.println("------------------------------------");
         System.out.println("\n\n\n\nThank you for buying a lottery ticket from Nana's and Ale's program :)");
     }
